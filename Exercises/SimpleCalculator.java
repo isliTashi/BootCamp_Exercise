9b7b27848d24d788c5d43003fd29eb5a07343d17
Isli_Tashi <148678075+isliTashi@users.noreply.github.com>
2024-07-03 15:29:30 +0200

    Update SimpleCalculator.java

--- Exercises/SimpleCalculator.java
+++ Exercises/SimpleCalculator.java
@@ -37,4 +37,21 @@ public class SimpleCalculator {
             return 0.0;
         }
     }
+
+        public static void main(String[] args) {
+         SimpleCalculator calculator = new SimpleCalculator();
+
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+
+         
+        System.out.println("Addition: " + calculator.getAdditionResult());
+        System.out.println("Subtraction: " + calculator.getSubtractionResult());
+
+         
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("Multiplication: " + calculator.getMultiplicationResult());
+        System.out.println("Division: " + calculator.getDivisionResult());
+    }
 }
